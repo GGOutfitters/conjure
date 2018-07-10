43f433bb2e21f8be4dc413355f965aaa2b92bbe2
Andrew Todd <atodd@ggoutfitters.com>
2018-07-10 10:56:10 -0400

    Fix pymongo version requirement

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     install_requires=[
-        'pymongo>3.0',
+        'pymongo<3.7',
     ],
     platforms=['any'],
     classifiers=CLASSIFIERS,
